98a0124fa9d5a65793cf2a1cfa0db2899bb34a91
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2016-03-06 02:25:13 +0300

    Add Bootstrapper

---
+++ Orationi.RichManager/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Prism.Unity;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Orationi.RichManager.Views;
+
+namespace Orationi.RichManager
+{
+	public class Bootstrapper : UnityBootstrapper
+	{
+		protected override DependencyObject CreateShell()
+		{
+			return Container.Resolve<MainWindow>();
+		}
+
+		protected override void InitializeShell()
+		{
+			Application.Current.MainWindow.Show();
+		}
+
+		protected override void ConfigureModuleCatalog()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
