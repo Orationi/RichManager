5527fa5c372781c781a77f5a50b71e4b08d45cf1
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2016-03-06 02:33:05 +0300

    Add slaves module

---
+++ Orationi.RichManager.Slaves/ViewModels/SlavesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace Orationi.RichManager.Slaves.ViewModels
+{
+	public class SlavesViewModel : BindableBase
+	{
+		private string _title = "Slaves";
+
+		public string Title
+		{
+			get { return _title; }
+			set { SetProperty(ref _title, value); }
+		}
+	}
+}
---
+++ Orationi.RichManager.Slaves/Views/SlavesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Orationi.RichManager.Slaves.Views
+{
+	/// <summary>
+	/// Interaction logic for SlavesView.xaml
+	/// </summary>
+	public partial class SlavesView : UserControl
+	{
+		public SlavesView()
+		{
+			InitializeComponent();
+		}
+	}
+}
